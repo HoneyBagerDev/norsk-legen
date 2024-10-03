948f9f2aa41b2d7976a2334c38477a5d6591bde4
HoneyBagerDev <user27384@proton.me>
2024-10-03 15:55:49 +0200

    base url changed

--- tina/config.ts
+++ tina/config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   build: {
     outputFolder: "/admin",
     publicFolder: "/static",
-    basePath: "/Norsk-lege/",
+    basePath: "Norsk-lege",
   },
   media: {
     tina: {
