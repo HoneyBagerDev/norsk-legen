60e47e74ac474af4f2713c0f83d502f9dd91a194
HoneyBagerDev <user27384@proton.me>
2024-10-19 21:15:09 +0200

    hugo, tina rebuild. reason: rename of repository

--- tina/config.ts
+++ tina/config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   build: {
     outputFolder: "admin",
     publicFolder: "static",
-    basePath: "Norsk-lege",
+    basePath: "norsk-legen",
   },
   media: {
     tina: {
